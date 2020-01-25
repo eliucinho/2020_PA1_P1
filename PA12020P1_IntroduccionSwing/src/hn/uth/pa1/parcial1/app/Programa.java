1473d6e095bc0e49ffe67898264ab74d27e01f72
eliucinho <adelman.varela@gmail.com>
2020-01-25 12:46:57 -0600

    Guardado inicial del proyecto

---
+++ PA12020P1_IntroduccionSwing/src/hn/uth/pa1/parcial1/app/Programa.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hn.uth.pa1.parcial1.app;
+
+/**
+ *
+ * @author uth
+ */
+public class Programa {
+    public static void main(String [] args){
+        System.out.println("hola mundo");
+        System.out.println("hola uth");
+    }
+}
